3438e3dd6c5b8518a9a0fa6c746ce0ae235dd61e
Huy Ha <huyha2016@gmail.com>
2023-03-02 23:06:46 -0800

    add reponsive mobile

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
+
 document.getElementById("compose-section").style.display = "none";
 
-document.getElementById("compose-btn").addEventListener('click', toggleSection);
+document.getElementById("compose-btn").addEventListener('click', showCompose);
 
 document.getElementById("close-compose").addEventListener('click', closeCompose);
 
-function toggleSection() {
+document.getElementById("nav-hamburger").addEventListener('click', showNav);
+
+document.getElementById("close-nav").addEventListener('click', closeNav);
+
+let width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+console.log(width);
+
+if (width < 768) {
+    console.log(width);
+    document.getElementById("nav-section").style.display = "none";
+}
+
+function showCompose() {
     document.getElementById("compose-section").style.display = "block";
+    width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    if (width < 768) {
+        document.getElementById("preview").style.display = "none";
+    }
+    if (width < 992) {
+        document.getElementById("preview").style.display = "none";
+    }
 }
 
 function closeCompose() {
     document.getElementById("compose-section").style.display = "none";
+    width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+    
+    document.getElementById("preview").style.display = "block";
+    
+}
+
+// Show nav-bar in mobile mode
+function showNav() {
+    document.getElementById("nav-section").style.display = "block";
+}
+
+// Hide nav-bar in mobile mode
+function closeNav() {
+    document.getElementById("nav-section").style.display = "none";
 }
